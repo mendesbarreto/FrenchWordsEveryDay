8ed104635ea90242135c874904140d5769273e67
Douglas Mendes Barreto <mendes-barreto@live.com>
2015-10-22 20:54:30 -0200

    Files

---
+++ FrenchWordsEveryDay/French Words Every Day/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenchWordsEveryDay.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        
+    }
+}
